a311b4b4a1810eb971d057277cc8f0b67e6614ac
loukik2912 <laukpb@gmail.com>
2020-09-21 17:03:25 +0530

    Task pushing Maven project

---
+++ src/test/java/com/db/sampleapp/SampleAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.db.sampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		assertTrue(true);
+	}
+
+}
